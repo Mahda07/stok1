e7a99d4724ea5a05a07876ce749b089e70081a7b
Mahda <mahdafatihah@gmail.com>
2026-02-04 13:05:30 +0700

    update main.js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 
 // Konfigurasi Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCfqZD7UZZt-GWmtNhfJyksrv3-8ENRjto",
-  authDomain: "insan-cemerlang-d5574.firebaseapp.com",
-  projectId: "insan-cemerlang-d5574",
-  storageBucket: "insan-cemerlang-d5574.appspot.com",
-  messagingSenderId: "1035937160050",
-  appId: "1:1035937160050:web:6d77d3874c3f78b2811beb",
-  measurementId: "G-EVVQ80Q08C"
+    apiKey: "AIzaSyCfqZD7UZZt-GWmtNhfJyksrv3-8ENRjto",
+    authDomain: "insan-cemerlang-d5574.firebaseapp.com",
+    projectId: "insan-cemerlang-d5574",
+    storageBucket: "insan-cemerlang-d5574.appspot.com",
+    messagingSenderId: "1035937160050",
+    appId: "1:1035937160050:web:6d77d3874c3f78b2811beb",
+    measurementId: "G-EVVQ80Q08C"
 };
 
 // Inisialisasi Firebase
